106f5e080137e2cfa5c8d766fbdb285065b5da78
Thillai1701 <Shansenthil72@gmail.com>
2025-09-16 21:03:38 +0530

    Added Upto HashTable 20250916

---
+++ 02.CODE SAMPLES/02.NON GENRIC COLLECTION/02_NG COLLECTION/02_HashTable/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+
+namespace _02_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01. Hashtable Definition
+            /*
+             * Hashtable (Non-Generic Collection)
+             * - Namespace: System.Collections
+             * - Stores data as Key-Value pairs (object type).
+             * - Key must be unique, Value can be duplicate.
+             * - Unordered collection → items are not stored in insertion order.
+             * - Allows different data types for Key & Value (since object type).
+             * - Similar to Dictionary<TKey, TValue> but non-generic.
+             */
+            #endregion
+
+            #region 02. Initialization of Hashtable
+            Hashtable Country = new Hashtable(); // Empty Hashtable
+            #endregion
+
+            #region 03. Adding Elements
+            Country.Add("IND", "India");                     // Add key-value
+            Country.Add("USA", "United States of America");
+            Country.Add("PAK", "Pakistan");
+            Country.Add("AUS", "Australia");
+            Country["ENG"] = "England";                      // Another way to insert/update
+            #endregion
+
+            #region 04. Count of Elements
+            Console.WriteLine("Total Countries: " + Country.Count);
+            #endregion
+
+            #region 05. Existence Check
+            Console.WriteLine("\nCheck existence:");
+            Console.WriteLine("Contains 'IND'? " + Country.Contains("IND"));         // True
+            Console.WriteLine("ContainsKey 'AUS'? " + Country.ContainsKey("AUS"));   // True
+            Console.WriteLine("ContainsValue 'India'? " + Country.ContainsValue("India")); // True
+            #endregion
+
+            #region 06. Remove Elements
+            Country.Remove("USA"); // Remove by Key
+            Console.WriteLine("\nAfter removing 'USA', total count: " + Country.Count);
+            #endregion
+
+            #region 07. Access Elements by Key
+            Console.WriteLine("\nAccess element by key 'IND': " + Country["IND"]);
+            #endregion
+
+            #region 08. Looping Through Hashtable
+            Console.WriteLine("\nLoop using foreach (DictionaryEntry):");
+            foreach (DictionaryEntry entry in Country)
+            {
+                Console.WriteLine("Key = " + entry.Key + ", Value = " + entry.Value);
+            }
+
+            Console.WriteLine("\nLoop through Keys:");
+            foreach (var key in Country.Keys)
+            {
+                Console.WriteLine("Key = " + key + ", Value = " + Country[key]);
+            }
+
+            Console.WriteLine("\nLoop through Values:");
+            foreach (var val in Country.Values)
+            {
+                Console.WriteLine("Value = " + val);
+            }
+            #endregion
+
+            #region 09. Clone Hashtable
+            Hashtable IPL = (Hashtable)Country.Clone(); // Shallow copy
+            Console.WriteLine("\nCloned Hashtable count: " + IPL.Count);
+            #endregion
+
+            #region 10. CopyTo Example
+            object[] keys = new object[Country.Count];
+            object[] values = new object[Country.Count];
+
+            Country.Keys.CopyTo(keys, 0);
+            Country.Values.CopyTo(values, 0);
+
+            Console.WriteLine("\nCopied Keys:");
+            foreach (var key in keys)
+                Console.Write(key + " ");
+
+            Console.WriteLine("\nCopied Values:");
+            foreach (var val in values)
+                Console.Write(val + " ");
+            Console.WriteLine();
+            #endregion
+        }
+    }
+}
---
+++ 02.CODE SAMPLES/02.NON GENRIC COLLECTION/02_NG COLLECTION/1_ARRAYLIST/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+
+namespace _1_ARRAYLIST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01. ArrayList Definition
+            /*
+             * ArrayList is a non-generic collection in C# that can hold 
+             * multiple types of data (objects). 
+             * - Namespace: System.Collections
+             * - Stores elements as object type.
+             * - Dynamic size (grows/shrinks as needed).
+             * - Allows duplicate elements.
+             * - Index based access (like arrays).
+             */
+            #endregion
+
+            #region 02. Initialization of ArrayList
+            ArrayList cricketPlayer = new ArrayList();                // Empty ArrayList
+            ArrayList arrayList_1 = new ArrayList(cricketPlayer);     // Copy from another ArrayList
+            ArrayList arrayList_2 = new ArrayList(50);                // Initialize with capacity 50
+            #endregion
+
+            #region 03. Adding Elements
+            cricketPlayer.Add("Rohit");                // Add string
+            cricketPlayer.Add(10);                     // Add int
+            cricketPlayer.Add("Sachin");               // Add another string
+            cricketPlayer.AddRange(arrayList_1);       // Add all elements from another ArrayList
+            #endregion
+
+            #region 04. Access Elements
+            Console.WriteLine("First element: " + cricketPlayer[0]); // Direct index access
+            #endregion
+
+            #region 05. Looping through ArrayList
+            Console.WriteLine("\nUsing for loop:");
+            for (int i = 0; i < cricketPlayer.Count; i++)
+            {
+                Console.WriteLine(cricketPlayer[i]);
+            }
+
+            Console.WriteLine("\nUsing foreach loop:");
+            foreach (var player in cricketPlayer)
+            {
+                Console.WriteLine(player);
+            }
+            #endregion
+
+            #region 06. Clear Elements
+            arrayList_1.Clear();
+            Console.WriteLine("\nCount of ArrayList 1 after Clear(): " + arrayList_1.Count);
+            #endregion
+
+            #region 07. Check Existence
+            Console.WriteLine("\nContains 10? " + cricketPlayer.Contains(10)); // True/False
+            #endregion
+
+            #region 08. Insert Elements
+            cricketPlayer.Insert(0, 45);                      // Insert at index 0
+            cricketPlayer.InsertRange(1, new ArrayList()      // Insert range after index 0
+            {
+                "Virat", "Dhoni"
+            });
+            #endregion
+
+            #region 09. Remove Elements
+            /*
+             * Remove(value)     → Removes first occurrence of the given value
+             * RemoveAt(index)   → Removes element at given index
+             * RemoveRange(start, count) → Removes multiple elements
+             */
+
+            Console.WriteLine("\nBefore Removing:");
+            foreach (var item in cricketPlayer)
+                Console.Write(item + " ");
+
+            cricketPlayer.Remove(10);        // Removes value "10"
+            cricketPlayer.RemoveAt(0);       // Removes element at index 0
+            if (cricketPlayer.Count >= 2)
+                cricketPlayer.RemoveRange(0, 2); // Removes 2 elements starting at index 0
+
+            Console.WriteLine("\n\nAfter Removing:");
+            foreach (var item in cricketPlayer)
+                Console.Write(item + " ");
+            Console.WriteLine();
+            #endregion
+
+            #region 10. Clone ArrayList
+            ArrayList arrayList_3 = (ArrayList)cricketPlayer.Clone(); // Creates a shallow copy
+            Console.WriteLine("\nCloned ArrayList count: " + arrayList_3.Count);
+            #endregion
+
+            #region 11. Sort ArrayList
+            /*
+             * Sorting works only if all elements are of the same type.
+             * Otherwise, InvalidOperationException will be thrown.
+             * So here, we’ll create a new ArrayList with same-type elements.
+             */
+            ArrayList numbers = new ArrayList() { 5, 2, 8, 1, 3 };
+            numbers.Sort();
+            Console.WriteLine("\nSorted Numbers:");
+            foreach (var num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region 12. CopyTo Example
+            object[] array = new object[cricketPlayer.Count];
+            cricketPlayer.CopyTo(array, 0);
+            Console.WriteLine("\nCopied Array Elements:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            #endregion
+        }
+    }
+}
