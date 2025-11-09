038d20a3ff0ddb3cc064c40d0f91adb57fb47464
Thillai1701 <Shansenthil72@gmail.com>
2025-11-09 10:08:49 +0530

    Add

---
+++ 02.CODE SAMPLES/05.FILE SYSTEM/File_Directory_info/File_Directory_Info/DirectoryInfo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryInfoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"T:\02.GIT REPO\01.COURSE-DOCUMENT\1_SAR ZIPFILE";
+            DirectoryInfo di = new DirectoryInfo(directory);
+
+            Console.WriteLine(di.Name);
+            Console.WriteLine(di.FullName);
+            Console.WriteLine(di.Exists);
+            Console.WriteLine(di.Attributes);
+            Console.WriteLine(di.LastAccessTime);
+
+
+            FileInfo[] Files = di.GetFiles();
+            DirectoryInfo[] df= di.GetDirectories();
+        }
+    }
+}
---
+++ 02.CODE SAMPLES/05.FILE SYSTEM/File_Directory_info/File_Directory_Info/FileInfoClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName = @"T:\02.GIT REPO\01.COURSE-DOCUMENT\1_SAR ZIPFILE\SARZIP.zip";
+            FileInfo fi = new FileInfo(FileName);
+
+            Console.WriteLine(fi.Name);
+            Console.WriteLine(fi.FullName);
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.DirectoryName);
+            Console.WriteLine(fi.CreationTime);
+            Console.WriteLine(fi.LastAccessTime);
+            Console.WriteLine(fi.Attributes);
+
+            fi.Delete();
+            fi.Create();
+            
+        }
+    }
+}
